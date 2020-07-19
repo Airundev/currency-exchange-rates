325994e7fa8e08c6113a882dbf45e1c7a3e1ccd5
Airun <nuriaskies@gmail.com>
2020-07-19 10:30:19 +0200

    WIP - Implement databinding

---
+++ app/src/main/java/com/example/currencyexchangerates/ui/components/CurrencyListCell.kt
@@ -0,0 +1,15 @@
+package com.example.currencyexchangerates.ui.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CurrencyListCell @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    FrameLayout(context, attrs, defStyleAttr) {
+
+
+}
---
+++ app/src/main/java/com/example/currencyexchangerates/ui/components/CurrencyListCellItem.kt
@@ -0,0 +1,8 @@
+package com.example.currencyexchangerates.ui.components
+
+data class CurrencyListCellItem (
+    val image: Int,
+    val title: String,
+    val description: String,
+    val value: Double
+)
