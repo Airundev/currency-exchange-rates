10e3eea1b8273d2faf73b284fd9f2b0e0b635d1a
Airun <nuriaskies@gmail.com>
2020-07-20 16:01:03 +0200

    WIP - Create recycler view and set adapter

--- app/src/main/java/com/example/currencyexchangerates/ui/main/CurrencyRateActivity.kt
+++ app/src/main/java/com/example/currencyexchangerates/ui/main/CurrencyRateActivity.kt
@@ -3,14 +3,27 @@ package com.example.currencyexchangerates.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.currencyexchangerates.R
+import com.example.currencyexchangerates.ui.components.CurrencyListCellAdapter
+import com.example.currencyexchangerates.ui.components.CurrencyListCellItem
+import kotlinx.android.synthetic.main.main_activity.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class CurrencyRateActivity : AppCompatActivity() {
+class CurrencyRateActivity : AppCompatActivity(), CurrencyListCellAdapter.CurrencyListCellListener {
 
     private val currencyRateViewModel: CurrencyRateViewModel by viewModel()
+
+    private lateinit var adapter: CurrencyListCellAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        adapter = CurrencyListCellAdapter()
+        recyclerView.adapter = adapter
+        adapter.listener = this
+    }
+
+    override fun onCurrencyCellClicked(item: CurrencyListCellItem) {
+        adapter.onCellClicked(item)
     }
 }
